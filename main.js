56da1fbf77c1e2d9b7b13839a7e8b4e38fff02a7
Gilles Piou <gilles.piou@gmail.com>
2018-03-24 15:07:18 +0800

    Start Preferences page

--- main.js
+++ main.js
@@ -1,17 +1,97 @@
 import path from "path";
 import url from "url";
-import { app, BrowserWindow, ipcMain, nativeImage, Tray } from "electron";
+import {
+  app,
+  BrowserWindow,
+  ipcMain,
+  Menu,
+  nativeImage,
+  shell,
+  Tray
+} from "electron";
 
 let tray = null;
 let canvas = null;
+let preferences = null;
 
 app.on("ready", () => {
   createCanvas();
   createTray();
 });
 
+ipcMain.on("Canvas", (event, params) => {
+  const im = nativeImage.createFromDataURL(params.dataURL);
+  const i = im.crop({
+    x: 0,
+    y: 0,
+    height: im.getSize().height,
+    width: Math.ceil(params.width)
+  });
+  tray.setImage(i.resize({ height: 32 }));
+});
+
 function createTray() {
   tray = new Tray(nativeImage.createEmpty());
+  tray.setContextMenu(createTrayMenu());
+}
+
+function createTrayMenu() {
+  return Menu.buildFromTemplate([
+    {
+      label: "Preferences...",
+      click: createPreferencesWindow
+    },
+    {
+      type: "separator"
+    },
+    {
+      label: "Check for update...",
+      click: function() {}
+    },
+    {
+      enabled: false,
+      label: `Current version: ${app.getVersion()}`
+    },
+    {
+      type: "separator"
+    },
+    {
+      label: "Help",
+      click: function() {
+        shell.openExternal("https://www.github.com/pioug/coinbae");
+      }
+    },
+    {
+      type: "separator"
+    },
+    {
+      label: "Quit Coinbae",
+      role: "quit"
+    }
+  ]);
+}
+
+function createPreferencesWindow() {
+  if (preferences) {
+    preferences.show();
+    return;
+  }
+
+  preferences = new BrowserWindow();
+  preferences.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "preferences.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+  if (process.env.DEBUG) {
+    preferences.webContents.openDevTools();
+  }
+
+  preferences.on("close", () => {
+    preferences = null;
+  });
 }
 
 function createCanvas() {
@@ -32,14 +112,3 @@ function createCanvas() {
     canvas = null;
   });
 }
-
-ipcMain.on("Canvas", (event, params) => {
-  const im = nativeImage.createFromDataURL(params.dataURL);
-  const i = im.crop({
-    x: 0,
-    y: 0,
-    height: im.getSize().height,
-    width: Math.ceil(params.width)
-  });
-  tray.setImage(i.resize({ height: 32 }));
-});
